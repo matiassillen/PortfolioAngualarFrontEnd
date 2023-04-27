e0f79b7f4319076163fd59a3b5a371b0698a69d1
Matias <matias.sillen@gmail.com>
2023-04-27 00:29:07 -0300

    Inyecto en el constructor del servicio el modulo Cliente Http

--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -1,11 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   obtenerDatos() {
     console.log("El servicio Portfolio está corriendo");
