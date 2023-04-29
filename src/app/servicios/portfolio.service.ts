ab527fee39d35e167689bc1bb8882940078d3c7c
Matias <matias.sillen@gmail.com>
2023-04-29 20:06:17 -0300

    Borré el metodo obtenerDatos del archivo portfolio.service.ts

--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -1,15 +1,11 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
 
-  constructor(private http:HttpClient) { }
+  constructor() { }
 
-  obtenerDatos():Observable<any> {
-    return this.http.get("json");
-  }
 }
