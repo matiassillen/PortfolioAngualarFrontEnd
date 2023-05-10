fd50b108ccd0f047231b750b818edf93ea3f0113
Matias <matias.sillen@gmail.com>
2023-05-10 20:02:12 -0300

    Configuré el app-routing-module y realicé cambios en los componentes header, login, redes-sociales, home y app-component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
 
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'login', component: LoginComponent}
 
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/componentes/redes-sociales/redes-sociales.component.ts
+++ src/app/componentes/redes-sociales/redes-sociales.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-redes-sociales',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./redes-sociales.component.css']
 })
 export class RedesSocialesComponent {
+
+  constructor(private router:Router) {}
+
+  ngOninit(): void {
+  }
+
+  login(){
+    this.router.navigate(['/login'])
+  }
 
 }
