78cc36eedd8159dc492bba06b1416994187adcf3
Matias <matias.sillen@gmail.com>
2023-05-15 23:44:47 -0300

    modificaciones

--- src/app/service/image.service.ts
+++ src/app/service/image.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Storage, ref } from '@angular/fire/storage'; 
+// import { Storage, ref } from '@angular/fire/storage'; 
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +9,23 @@ export class ImageService {
   constructor(private storage: Storage) { }
 
   public uploadImage($event: any, name: string) {
-    const file = $event.target.files[0]
-  const imgRef = ref(this.storage, 'imagen/'+ name)
-  upLoadBytes(imgRef, file)
-  .then(response => {this.getImages()})
-  .catch(error => console.log(error))
+  //   const file = $event.target.files[0]
+  // const imgRef = ref(this.storage, 'imagen/'+ name)
+  // upLoadBytes(imgRef, file)
+  // .then(response => {this.getImages()})
+  // .catch(error => console.log(error))
  }
 
   getImages(){
-    constante imagesRef = ref(this.storage, 'imagen')
-    list(imagesRef)
-    .then(async response => {
-    for(let item for respinse.items) {
-      this.url = await getDowloadURL(item);
-      console.log("La URL es: " + this.url);
-      }
-   })
-   .catch(error => console.log(error))
+  //   constante imagesRef = ref(this.storage, 'imagen')
+  //   list(imagesRef)
+  //   .then(async response => {
+  //   for(let item for respinse.items) {
+  //     this.url = await getDowloadURL(item);
+  //     console.log("La URL es: " + this.url);
+  //     }
+  //  })
+  //  .catch(error => console.log(error))
  }
 }
 
